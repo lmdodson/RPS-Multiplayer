710e3438d36b4c174b8edcd14e7e72db43e3737a
Leah Dodson <leahdodson9@gmail.com>
2020-01-21 23:19:16 -0600

    added css reset and initial html for login, added firebase config info for js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpIXz-Em9NbDZj8GxZ5WqL-GgKLsIjhVg",
+    authDomain: "rpsgame-10057.firebaseapp.com",
+    databaseURL: "https://rpsgame-10057.firebaseio.com",
+    projectId: "rpsgame-10057",
+    storageBucket: "rpsgame-10057.appspot.com",
+    messagingSenderId: "953615677798",
+    appId: "1:953615677798:web:ff0e1c75c0045d52edb0a7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
